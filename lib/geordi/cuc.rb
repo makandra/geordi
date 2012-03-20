86063efa8c958fac1e248a307cb84f7075530980
Ulrich Berkmüller <ulrich.berkmueller@makandra.de>
2012-03-20 17:45:19 +0100

    fix type of features to run output

--- lib/geordi/cuc.rb
+++ lib/geordi/cuc.rb
@@ -59,7 +59,7 @@ module Geordi
 
     def show_features_to_run
       unless features_to_run.empty?
-        passed_by = (features_to_run == command_line_features) ? 'command line' : 'rerun.txt'
+        passed_by = (features_to_run == rerun_txt_features && features_to_run != command_line_features) ? 'rerun.txt' : 'command line'
         2.times { puts }
         puts "features to run (passed by #{passed_by}):"
         puts "-----------------------------------------"
