574eb50ec9c6305e0725e7998f6a475c5c9b246f
Dominik Schöler <dominik.schoeler@makandra.de>
2014-05-21 16:21:09 +0200

    Install missing gems instead of dying

--- lib/geordi/util.rb
+++ lib/geordi/util.rb
@@ -5,10 +5,30 @@ module Geordi
     class << self
       include Geordi::Interaction
 
+      def installing_missing_gems(&block)
+        yield
+      rescue LoadError => e
+        gem_name = e.message.split('--').last.strip
+        install_command = 'gem install ' + gem_name
+
+        # install missing gem
+        warn 'Probably missing gem: ' + gem_name
+        system! install_command, :show_cmd => true
+
+        # retry
+        Gem.clear_paths
+        require gem_name
+        note 'Trying again ...'
+        retry
+      end
+
       # Run a command with a clean environment.
       # Print an error message and exit if the command fails.
+      #
+      # Options: show_cmd, fail_message
       def system!(*commands)
         options = commands.last.is_a?(Hash) ? commands.pop : {}
+        note_cmd commands.join(' ') if options[:show_cmd]
 
         # Remove the gem's Bundler environment when running commands.
         Bundler.clean_system(*commands) or fail(options[:fail_message] || 'Something went wrong.')
