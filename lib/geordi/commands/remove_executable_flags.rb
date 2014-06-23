c07674e4939c1ad05de7ce3c38305807dae00071
Dominik Schöler <dominik.schoeler@makandra.de>
2014-06-23 17:06:40 +0200

    Also remove x-flags from Feature files

--- lib/geordi/commands/remove_executable_flags.rb
+++ lib/geordi/commands/remove_executable_flags.rb
@@ -2,7 +2,10 @@ desc 'remove_executable_flags', 'Remove executable-flags from files that should 
 def remove_executable_flags
   announce 'Removing executable-flags'
 
-  patterns = %w[ *.rb *.html *.erb *.haml *.yml *.css *.sass *.rake *.png *.jpg *.gif *.pdf *.txt *.rdoc Rakefile VERSION README Capfile ]
+  patterns = %w[
+    *.rb *.html *.erb *.haml *.yml *.css *.sass *.rake *.png *.jpg
+    *.gif *.pdf *.txt *.rdoc *.feature Rakefile VERSION README Capfile
+  ]
   for pattern in patterns
     note pattern
     `find . -name "#{pattern}" -exec chmod -x {} ';'`
