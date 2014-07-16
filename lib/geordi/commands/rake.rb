ab245da8b9bdbd3002a82c80395c779d97d2d2c1
Dominik Schöler <dominik.schoeler@makandra.de>
2014-07-16 11:46:32 +0200

    Fix rake command

--- lib/geordi/commands/rake.rb
+++ lib/geordi/commands/rake.rb
@@ -2,7 +2,7 @@ desc 'rake', 'Run rake tasks in all Rails environments'
 def rake(*args)
   for env in %w[development test cucumber performance]
     if File.exists? "config/environments/#{env}.rb"
-      call = ['bundle exec rake'] + args + ["RAILS_ENV=#{env}"]
+      call = %w[bundle exec rake] + args + ["RAILS_ENV=#{env}"]
       note_cmd call.join(' ')
 
       Util.system! *call
