734a48334b2121da91e42e53d826d783840e48e8
Dominik Schöler <dominik.schoeler@makandra.de>
2015-01-12 13:49:56 +0100

    Fix hiding output of bundle_install command

--- lib/geordi/commands/bundle_install.rb
+++ lib/geordi/commands/bundle_install.rb
@@ -1,6 +1,6 @@
 desc 'bundle_install', 'Run bundle install if required', :hide => true
 def bundle_install
-  if File.exists?('Gemfile') and !system('bundle check > /dev/null')
+  if File.exists?('Gemfile') and !system('bundle check > /dev/null 2>&1')
     announce 'Bundling'
     Util.system! 'bundle install'
   end
