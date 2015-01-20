2955003513b4e4c15f3d76025347aea868eb7263
Dominik Schöler <dominik.schoeler@makandra.de>
2015-01-20 10:05:06 +0100

    Improve security update command

---
+++ lib/geordi/commands/security_update.rb
@@ -0,0 +1,54 @@
+desc 'security-update [step]', 'Support for performing security updates'
+long_desc <<-LONGDESC
+Will prepare a project for a security update when called without arguments, or
+with 'prepare'.
+
+Will finish a security update when called with 'finish'.
+LONGDESC
+
+def security_update(step='prepare')
+  case step
+  when 'prepare'
+    announce 'Preparing for security update'
+    warn 'Please read https://makandracards.com/makandra/1587 before applying security updates!'
+    note 'About to: pull master and production branches, checkout production'
+    wait 'Continue?'
+
+    Util.system! 'git checkout master', :show_cmd => true
+    Util.system! 'git pull', :show_cmd => true
+    Util.system! 'git checkout production', :show_cmd => true
+    Util.system! 'git pull', :show_cmd => true
+
+    success 'Successfully prepared for security update'
+    puts
+    note 'Please apply the security update now.'
+    note 'When you are done, run `geordi security-update finish`.'
+
+
+  when 'finish'
+    announce 'Finishing security update'
+
+    # ensure everything is committed
+    `git status --porcelain`.empty? or fail('There are uncommitted changes.')
+    note 'Working directory clean.'
+
+    print 'Have you successfully run all tests? [yN] '
+    exit unless $stdin.gets =~ /[yes]+/
+
+    note 'About to: push production, checkout & pull master, merge production, push master, deploy all stages'
+    wait 'Continue?'
+
+    Util.system! 'git push', :show_cmd => true
+    Util.system! 'git checkout master', :show_cmd => true
+    Util.system! 'git pull', :show_cmd => true
+    Util.system! 'git merge production', :show_cmd => true
+    Util.system! 'git push', :show_cmd => true
+
+    invoke_cmd 'all_targets', 'deploy:migrations'
+
+    success 'Successfully pushed and deployed security update'
+    puts
+    note 'Now send an email to customer and project lead, informing them about the update.'
+    note 'Do not forget to make a joblog on a security budget, if available.'
+  end
+end
