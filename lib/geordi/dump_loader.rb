804342ca402ab6015cd97975fab0f37d6e4df296
Dominik Schöler <dominik.schoeler@makandra.de>
2016-02-02 11:32:21 +0100

    Restore files that were accidentally deleted

---
+++ lib/geordi/dump_loader.rb
@@ -0,0 +1,60 @@
+require 'rubygems'
+require 'highline'
+require 'geordi/interaction'
+require 'geordi/util'
+
+module Geordi
+  class DumpLoader
+    include Geordi::Interaction
+
+    def initialize(file)
+      @dump_file = file
+    end
+
+    def development_database_config
+      require 'yaml'
+
+      @config ||= YAML::load(ERB.new(File.read('config/database.yml')).result)
+      @config['development']
+    end
+    alias_method :config, :development_database_config
+  
+    def mysql_command
+      command = 'mysql --silent'
+      command << ' -p' << config['password'] if config['password']
+      command << ' -u' << config['username'] if config['username']
+      command << ' --default-character-set=utf8'
+      command << ' ' << config['database']
+      command << ' < ' << dump_file
+    end
+    alias_method :mysql2_command, :mysql_command
+
+    def postgresql_command
+      ENV['PGPASSWORD'] = config['password']
+      command = 'pg_restore --no-owner --clean'
+      command << ' --username=' << config['username'] if config['username']
+      command << ' --host=' << config['host'] if config['host']
+      command << ' --dbname=' << config['database']
+      command << ' ' << dump_file
+    end
+
+    def dump_file
+      @dump_file ||= begin
+        dumps_glob = File.join(File.expand_path('~'), 'dumps', '*.dump')
+        available_dumps = Dir.glob(dumps_glob).sort
+
+        HighLine.new.choose(*available_dumps) do |menu|
+          menu.hidden('') { fail 'Abort.' }
+        end
+      end
+    end
+
+    def load
+      note 'Source file: ' + dump_file
+
+      source_command = send("#{config['adapter']}_command")
+      Util.system! source_command, :fail_message => "An error occured loading #{File.basename(dump_file)}"
+    end
+
+  end
+end
