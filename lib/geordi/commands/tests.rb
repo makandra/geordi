3017adc38ddc9955cdcae50981a202b37c37ced0
Dominik Schöler <dominik.schoeler@makandra.de>
2016-08-30 09:24:26 +0200

    tests command: Only run specs, features etc. if rake did not perform

--- lib/geordi/commands/tests.rb
+++ lib/geordi/commands/tests.rb
@@ -1,9 +1,14 @@
 desc 'tests', 'Run all employed tests'
 def tests
-  invoke_cmd 'with_rake'
-  invoke_cmd 'unit'
-  invoke_cmd 'rspec'
-  invoke_cmd 'cucumber'
+  rake_result = invoke_cmd 'with_rake'
+
+  if rake_result == :did_not_perform
+    # Since `rake` usually runs all tests, only run them here if `rake` did not
+    # perform
+    invoke_cmd 'unit'
+    invoke_cmd 'rspec'
+    invoke_cmd 'cucumber'
+  end
 
   success 'Successfully ran tests.'
 end
--- lib/geordi/commands/with_rake.rb
+++ lib/geordi/commands/with_rake.rb
@@ -1,11 +1,12 @@
 desc 'with-rake', 'Run tests with `rake`', :hide => true
 def with_rake
-  if file_containing?('Rakefile', /^task.+default.+(spec|test)/)
+  if file_containing?('Rakefile', /^task.+default.+(spec|test|feature)/)
     invoke_cmd 'bundle_install'
 
     announce 'Running tests with `rake`'
     Util.system! 'rake'
   else
     note '`rake` does not run tests.'
+    :did_not_perform
   end
 end
