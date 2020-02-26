efcfc9d621a3c95dfefd221d6e4aea960ffddea3
Henning Koch <henning.koch@makandra.de>
2020-02-26 09:05:54 +0100

    geordi dump: Compress dumps with gzip before download

--- lib/geordi/remote.rb
+++ lib/geordi/remote.rb
@@ -9,6 +9,7 @@ module Geordi
     include Geordi::Interaction
 
     REMOTE_DUMP_PATH = '~/dumps/dump_for_download.dump'
+    REMOTE_DUMP_PATH_GZ = "#{REMOTE_DUMP_PATH}.gz"
 
     def initialize(stage)
       @stage = stage
@@ -34,18 +35,21 @@ module Geordi
     def dump(options = {})
       # Generate dump on the server
       shell options.merge({
-        :remote_command => "dumple #{@config.env} --for_download",
+        :remote_command => "dumple #{@config.env} --for_download && gzip -f #{REMOTE_DUMP_PATH}",
         :select_server => nil # Dump must be generated on the primary server
       })
 
       destination_directory = File.join(@config.root, 'tmp')
       FileUtils.mkdir_p destination_directory
       destination_path = File.join(destination_directory, "#{@stage}.dump")
-      relative_destination = Pathname.new(destination_path).relative_path_from Pathname.new(@config.root)
+      destination_path_gz = "#{destination_path}.gz"
 
-      note "Downloading remote dump to #{relative_destination} ..."
+      relative_destination = Pathname.new(destination_path).relative_path_from Pathname.new(@config.root)
+      relative_destination_gz = "#{relative_destination}.gz"
+
+      note "Downloading remote dump to #{relative_destination_gz} ..."
       server = @config.primary_server
-      Util.system! "scp -C #{ @config.user(server) }@#{ server }:#{REMOTE_DUMP_PATH} #{destination_path}"
+      Util.system! "scp -C #{ @config.user(server) }@#{ server }:#{REMOTE_DUMP_PATH_GZ} #{destination_path_gz} && gunzip -f #{destination_path_gz}"
 
       success "Dumped the #{@stage} database to #{relative_destination}."
 
