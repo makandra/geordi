a28a651ba7ef6219b8bf2bedfdb001dcb00390d7
Niklas Hasselmeyer <niklas.hasselmeyer@makandra.de>
2020-07-22 14:49:05 +0200

    Remove eurest command (Closes #96)

--- lib/geordi/commands/eurest.rb
+++
@@ -1,4 +0,0 @@
-desc 'eurest', 'Open the current Eurest cantina menu', hide: true
-def eurest
-  Util.system! %(file="Speiseplan_KW`date +%V`.pdf" && wget -O/tmp/$file http://www.eurest-extranet.de/eurest/export/sites/default/sigma-technopark/de/downloads/$file && xdg-open /tmp/$file)
-end
