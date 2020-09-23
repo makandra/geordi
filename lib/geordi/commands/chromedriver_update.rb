ffbf909edec314524049717138915c830eec770d
Bruno Sedler <bruno.sedler@makandra.de>
2020-09-23 17:30:58 +0200

    added glocbal config option auto_update_chromedriver

--- lib/geordi/commands/chromedriver_update.rb
+++ lib/geordi/commands/chromedriver_update.rb
@@ -4,6 +4,10 @@ Example: `geordi chromedriver_update`
 
 This command will find and install the matching chromedriver for the currently
 installed Chrome.
+
+Setting `auto_update_chromedriver` to `true` in your global Geordi config file 
+(`~/.config/geordi/global.yml`), will automatically update chromedriver before 
+cucumber tests, in case Chrome and chromedriver versions don't match
 LONGDESC
 
 option :quiet_if_matching, type: :boolean, default: false,
