721eb8775e7af2624b1c65c0c25a7ac66808d8dc
Tobias Kraze <tobias.kraze@makandra.de>
2020-12-11 15:07:02 +0100

    docker shell stops container; docker shell --secondary attaches

--- features/step_definitions/docker_command_steps.rb
+++ features/step_definitions/docker_command_steps.rb
@@ -8,3 +8,14 @@ Given(/the docker command cannot find the "(.*?)" binary/) do |command|
     end
   end
 end
+
+Given(/^the docker command finds a running shell "(.*?)"$/) do |shell_name|
+  require 'geordi/docker'
+  expect_any_instance_of(Geordi::Docker).to receive(:mock_parse).at_least(:once).and_wrap_original do |original, *args|
+    if args[0] =~ /docker-compose ps/
+      "other_shell\nshell_name foo"
+    else
+      original.call(*args)
+    end
+  end
+end
