8f8eca0e1bf98bc591c77128bf1bdc9e6cb25779
Bruno Sedler <bruno.sedler@makandra.de>
2021-03-04 17:56:23 +0100

    Add 'ignore previous output' step and remove other output steps

--- features/step_definitions/aruba_steps.rb
+++ features/step_definitions/aruba_steps.rb
@@ -3,42 +3,6 @@ if Aruba::VERSION < '1.0.0'
 end
 require 'aruba/generators/script_file'
 
-Then /^the last (\d) lines of (output|stderr|stdout) should contain: "(.+?)"$/ do |line_count, channel, expected_output|
-  output = case channel
-  when 'output'
-    all_output
-  when 'stderr'
-    all_stderr
-  when 'stdout'
-    all_stdout
-  end
-
-  last_lines = last_lines_from_output(output, line_count)
-  expect(last_lines).to include(expected_output)
+Given 'I ignore previous output' do
+  aruba.command_monitor.clear
 end
-
-Then /^the last (\d) lines of (output|stderr|stdout) should contain:$/ do |line_count, channel, expected_output|
-  output = case channel
-  when 'output'
-    all_output
-  when 'stderr'
-    all_stderr
-  when 'stdout'
-    all_stdout
-  end
-
-  last_lines = last_lines_from_output(output, line_count)
-  expect(last_lines).to include(expected_output)
-end
-
-
-module ArubaHelper
-  def last_lines_from_output(output, line_count)
-    sanitized_array = sanitize_text(output).split(/\n/)
-    filtered = sanitized_array.reject(&:empty?)
-    filtered.last(line_count.to_i).join
-  end
-end
-
-World(ArubaHelper)
-
