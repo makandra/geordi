965844e2cd6288eea2db14b8fbb0cc9c9a77b695
Stephan Teichert <stephan.teichert@makandra.de>
2021-05-06 09:03:07 +0200

    [#177576666] Avoid exiting after invalid config keys are detected

--- features/step_definitions/aruba_steps.rb
+++ features/step_definitions/aruba_steps.rb
@@ -6,3 +6,8 @@ require 'aruba/generators/script_file'
 Given 'I ignore previous output' do
   aruba.command_monitor.clear
 end
+
+Then(/^(?:the )?output should contain '([^']*)' (\d) times?$/) do |expected,amount|
+  console_output = aruba.command_monitor.all_output
+  expect(console_output.scan(expected).length).to eq amount.to_i
+end
