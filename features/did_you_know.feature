c058902ca8372343c62ffe4f2d2f92052b608eb5
Dominik Schöler <dominik.schoeler@makandra.de>
2023-07-24 15:42:35 +0200

    Finalize suggestions feature

--- features/did_you_know.feature
+++ features/did_you_know.feature
@@ -5,4 +5,4 @@ Feature: Some commands print hints about related commands and options
     And a file named "tmp/global_settings.yml" with "hint_probability: 100"
 
     When I run `geordi rake`
-    Then the output should contain "Did you know? `geordi capistrano` can run a Capistrano command on all deploy targets"
+    Then the output should contain "Did you know? `geordi capistrano` can run a Capistrano command on all deploy targets."
--- lib/geordi/hint.rb
+++ lib/geordi/hint.rb
@@ -12,6 +12,7 @@ module Geordi
 
         generated_hints = hints.map(&method(:generate))
         if generated_hints.any? && should_print_hint
+          puts
           puts generated_hints.sample
           puts 'You can configure the probability for these hints by setting hint_probability to a unitless percent number in ~/.config/geordi/global.yml' unless settings_probability
         end
@@ -25,13 +26,13 @@ module Geordi
         if hint.is_a?(Symbol)
           command = Geordi::CLI.commands[hint.to_s]
           description = downcase_first_letter(command.description)
-          "Did you know? `geordi #{command.name}` can #{description}"
+          "Did you know? `geordi #{command.name}` can #{description}."
         elsif hint.is_a?(Array)
           command = Geordi::CLI.commands[hint[0].to_s]
           option = command.options[hint[1]]
           banner = option.banner.nil? ? '' : " #{option.banner}"
           description = downcase_first_letter(option.description)
-          "Did you know? `geordi #{command.name} #{option.aliases.first}#{banner}` can #{description}"
+          "Did you know? `geordi #{command.name} #{option.aliases.first}#{banner}` can #{description}."
         elsif hint.is_a?(String)
           "Did you know? #{hint}"
         else
