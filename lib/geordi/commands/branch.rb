1e0943dd61043149c6b9a2a33ba6f5038e75fcf6
Lisa hader <lisa.hader@makandra.de>
2024-10-07 11:30:30 +0200

    Update Geordi commands to use the new linear-api

--- lib/geordi/commands/branch.rb
+++ lib/geordi/commands/branch.rb
@@ -1,16 +1,16 @@
-desc 'branch', 'Check out a feature branch based on a story from Pivotal Tracker'
+desc 'branch', 'Check out a feature branch based on an issue from Linear'
 long_desc <<-LONGDESC
 Example: `geordi branch`
 
-On the first execution we ask for your Pivotal Tracker API token and for your Git user initials. Both will be
+On the first execution we ask for your Linear API token and for your Git user initials. Both will be
 stored in `~/.config/geordi/global.yml`.
 LONGDESC
 
 option :from_master, aliases: '-m', type: :boolean, desc: 'Branch from master instead of the current branch'
 
 def branch
-  require 'geordi/gitpt'
-  Gitpt.new.run_branch(from_master: options.from_master)
+  require 'geordi/gitlinear'
+  Gitlinear.new.run_branch(from_master: options.from_master)
 
   Hint.did_you_know [
     :commit,
