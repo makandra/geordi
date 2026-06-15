067ff5b8dbe2b64ebb45a8543cb8d106626a8782
Lisa Hader <lisa.hader@makandra.de>
2026-06-15 09:01:30 +0200

    [W-389] `geordi deploy`: only announce Linear issues that will actually be moved

--- spec/linear_client_spec.rb
+++ spec/linear_client_spec.rb
@@ -1,5 +1,18 @@
 RSpec.describe Geordi::LinearClient do
 
+  describe '#filter_existing_issue_ids' do
+    it 'returns only issue ids that are also present in linear' do
+      # override spec_helper default to test actual filtering logic.
+      # This is okay here because we mock the actual API call
+      allow(Geordi::Util).to receive(:testing?).and_return(false)
+
+      linear_issues = [{ 'identifier' => 'W-123' }, { 'identifier' => 'W-234' }]
+      allow(subject).to receive(:fetch_linear_issues).and_return(linear_issues)
+
+      expect(subject.filter_existing_issue_ids(%w[W-123 W-234 W-345 A-123])).to eq %w[W-123 W-234]
+    end
+  end
+
   describe '.extract_issue_ids' do
     it 'returns extracted issue ids (only from the beginning of the commit message to avoid false matches)' do
       commit_messages = ["first example commit", "[W-365] Linear Issue Commit", "Commit with id [A-123] that gets ignored"]
